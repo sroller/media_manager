36fed1653f30a79e4d55945fddc71a66c3609965
Steffen Roller <steffen.roller@gmail.com>
2019-12-11 12:55:59 -0500

    initial commit

---
+++ createdb.sql
@@ -0,0 +1,23 @@
+CREATE TABLE media (
+  tstmp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  path string,
+  filename string,
+  ext string,
+  size number,
+  mtime datetime,
+  digest string,
+  height int,
+  width int,
+  duration int,
+  datetime_original string,
+  lon text,
+  lat text,
+  camera text,
+  exif json);
+
+CREATE INDEX media_path ON media(path);
+CREATE INDEX media_digest ON media(digest);
+CREATE INDEX media_filename ON media(filename);
+
+.quit
+
